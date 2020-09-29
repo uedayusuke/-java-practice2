0b76737f09c45939eccc9d8956dea0861955b46d
uedayusuke <uedayusuke1107@gmail.com>
2020-09-29 19:14:08 +0900

    [ADD]21.Javaのお作法

--- test/src/info/serchman/HelloWorld.java
+++ test/src/info/serchman/HelloWorld.java
@@ -1,8 +1,18 @@
+// packageはフォルダ・ディレクトリ管理
 package info.serchman;
 
 // セクション3:Eclipseを試してみよう
+
+// プログラム名称の宣言「class + 名称」
+// 名称→単語ごとに大文字で区分ける
 public class HelloWorld {
+	// public→誰でも使える
+	// static→オブジェクトを作らなくても使える
+	// void→戻り値なし
+	// main→メソッド、プログラムを実行する管理、一番最初に実行される！
 	public static void main(String args[]) {
+		// 命令文のお尻には";"
+		// System.out.println("")→文字の出力
 		System.out.println("HelloWorld");
 	}
 }
