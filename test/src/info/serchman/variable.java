ecee4e3b9a5fcf7053b0314c5290237fca8ef379
uedayusuke <uedayusuke1107@gmail.com>
2020-09-30 19:13:13 +0900

    [ADD]23.変数の扱い

--- test/src/info/serchman/variable.java
+++ test/src/info/serchman/variable.java
@@ -3,11 +3,14 @@ package info.serchman;
 // 23.変数の扱い
 public class variable {
 	public static void main(String args[]) {
+		// int型(整数)の変数を宣言
 		int x;
-		int x1;
-		int x2;
-		x1 = 10;
-		x2 = 5;
+
+		// int型(整数)の変数に値を代入→省略できる！！
+		int x1 = 10;
+		int x2 = 5;
+
+		// x1とx2の合計をxに代入
 		x = x1 + x2;
 		System.out.println(x);
 		x = x1 / x2;
