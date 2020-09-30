6f3253011db7fbf74251fc64e835250a4ff18163
uedayusuke <uedayusuke1107@gmail.com>
2020-09-30 19:08:21 +0900

    [ADD]23.変数の扱い

--- test/src/info/serchman/variable.java
+++ test/src/info/serchman/variable.java
@@ -3,7 +3,15 @@ package info.serchman;
 // 23.変数の扱い
 public class variable {
 	public static void main(String args[]) {
-
+		int x;
+		int x1;
+		int x2;
+		x1 = 10;
+		x2 = 5;
+		x = x1 + x2;
+		System.out.println(x);
+		x = x1 / x2;
+		System.out.println(x);
 	}
 
 }
