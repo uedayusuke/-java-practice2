36ec542f2112691ae3aa4ab65d0d0b40ecd19ebc
uedayusuke <uedayusuke1107@gmail.com>
2020-10-01 17:10:55 +0900

    [ADD]24.型変換

--- test/src/info/serchman/Variable2.java
+++ test/src/info/serchman/Variable2.java
@@ -7,13 +7,17 @@ public class Variable2 {
 		String x;
 
 		// String型の変数を宣言し、文字列を代入→省略可能！
-		String x1 = "おはよう";
-		String x2 = "ございます";
+		String x1 = "10";
+		int y1;
+		y1 = Integer.parseInt(x1);
+		String x2 = "5";
+		int y2;
+		y2 = Integer.parseInt(x2);
 
 		//変数xにx1とx2を合わせたものを代入
 		x = x1 + x2;
 
 		System.out.println(x);
-		// System.out.println(x1 + x2); でも同様
+		System.out.println(y1 + y2);
 	}
 }
