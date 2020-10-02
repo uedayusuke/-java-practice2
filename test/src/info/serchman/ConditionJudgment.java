63bd7a4b96c42da494cd6708a632c43f8da16038
uedayusuke <uedayusuke1107@gmail.com>
2020-10-02 13:52:03 +0900

    [ADD]25.条件判断

--- test/src/info/serchman/ConditionJudgment.java
+++ test/src/info/serchman/ConditionJudgment.java
@@ -3,6 +3,21 @@ package info.serchman;
 // 25.条件判断
 public class ConditionJudgment {
 	public static void main(String args[]) {
+		// int型(整数)の変数umareを宣言
+		int umare;
 
+		// 変数umareに代入
+		umare = 1980;
+		/*
+		 条件判断の基本
+		 if(条件){処理}
+		 */
+		if (umare < 1989) {
+			System.out.println("昭和生まれです");
+		}
+		umare = 1990;
+		if (umare >= 1989) {
+			System.out.println("平成生まれです");
+		}
 	}
 }
