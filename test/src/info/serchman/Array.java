8ca5eead7f7ec5658dfd69f2c4b33ea5634cfb7a
uedayusuke <uedayusuke1107@gmail.com>
2020-10-05 20:13:43 +0900

    [ADD]27.配列

--- test/src/info/serchman/Array.java
+++ test/src/info/serchman/Array.java
@@ -4,17 +4,22 @@ package info.serchman;
 public class Array {
 	public static void main(String args[]) {
 		/*
+		 配列の宣言
 		 型 変数名[] = new 型[x]
 		 (xは表の行数)
 		 */
 		String name[] = new String[3];
 		double height[] = new double[3];
+
+		// 下記で値を代入
 		name[0] = "山田";
 		name[1] = "田中";
 		name[2] = "鈴木";
 		height[0] = 170.2;
 		height[1] = 183.2;
 		height[2] = 175.7;
+
+		// forで繰り返し
 		for (int i = 0; i <= 2; i++) {
 			System.out.println(name[i] + ":" + height[i] + "cm");
 		}
