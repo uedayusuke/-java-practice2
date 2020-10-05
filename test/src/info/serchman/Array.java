470c99085746abdc954fef5e01dbfa7baae88ddc
uedayusuke <uedayusuke1107@gmail.com>
2020-10-05 20:10:52 +0900

    [ADD]27.配列

--- test/src/info/serchman/Array.java
+++ test/src/info/serchman/Array.java
@@ -3,6 +3,16 @@ package info.serchman;
 // 27.配列
 public class Array {
 	public static void main(String args[]) {
-
+		String name[] = new String[3];
+		double height[] = new double[3];
+		name[0] = "山田";
+		name[1] = "田中";
+		name[2] = "鈴木";
+		height[0] = 170.2;
+		height[1] = 183.2;
+		height[2] = 175.7;
+		for (int i = 0; i <= 2; i++) {
+			System.out.println(name[i] + ":" + height[i] + "cm");
+		}
 	}
 }
